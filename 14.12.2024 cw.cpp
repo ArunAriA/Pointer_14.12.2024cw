759f1c1fe7bfde6962624d014981e1bd01b96d59
ArunAriA <kasik228483895@gmail.com>
2024-12-14 16:00:47 +0200

    1 Zavdanya

--- 14.12.2024 cw.cpp
+++ 14.12.2024 cw.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+int swapedArr(int size, int* arr)
+{
+    int counter = 1;
+
+    for (int i = 0; i < size / 2; i++)
+    {
+
+        int s = *(arr + i * 2);
+        *(arr + i * 2) = *(arr + 1 + i * 2);
+        *(arr + 1 + i * 2) = s;
+        
+    }
+
+    return 0;
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    srand(time(NULL));
+
+    const int size = 10;
+    int arr[size];
+    int *ptr = arr;
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10;
+        cout << arr[i] << ' ';
+    }
+
+    cout << endl;
+
+    swapedArr(size, ptr);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+
 }
