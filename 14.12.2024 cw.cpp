3f1740a1620fdcac3b87087ff02aa0746d2395a9
ArunAriA <kasik228483895@gmail.com>
2024-12-14 17:58:24 +0200

    kinec

--- 14.12.2024 cw.cpp
+++ 14.12.2024 cw.cpp
@@ -60,72 +60,103 @@ using namespace std;
 
 
 
-void enterNum(int size, int* arr)
-{
-	for (int i = 0; i < size; i++)
-	{
-		cin >> *(arr + i);
-	}
-}
+//void enterNum(int size, int* arr)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		cin >> *(arr + i);
+//	}
+//}
+//
+//void printF(int size, int* arr)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << *(arr + i) << ' ';
+//	}
+//}
+//
+//int printMenu(int size, int *arr, int* arr2, int sellect)
+//{
+//	int index = 0;
+//	
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (sellect == 1)
+//		{
+//			if (*(arr + i) % 2 != 0)
+//			{
+//				*(arr2 + index) = *(arr + i);
+//				index++;
+//			}
+//		}
+//		if (sellect == 2)
+//		{
+//			if (*(arr + i) % 2 == 0)
+//			{
+//				*(arr2 + index) = *(arr + i);
+//				index++;
+//			}
+//		}
+//	}
+//
+//	return index;
+//}
+//
+//// 2 parne
+//
+//int main()
+//{
+//	cout << "Enter massive size: ";
+//	int size;
+//	cin >> size;
+//
+//	int* arr = new int[size];
+//	
+//	int* arr2 = new int[size];
+//	
+//	enterNum(size, arr);
+//	printF(size, arr);
+//	cout << endl << "Select delete num - 1 ne panrni / 2 parni: " << endl;
+//	int sel;
+//	cin >> sel;
+//	
+//	int index = printMenu(size, arr, arr2, sel);
+//	printF(index, arr2);
+//
+//
+//	delete[] arr;
+//	delete[] arr2;
+//}
 
-void printF(int size, int* arr)
-{
-	for (int i = 0; i < size; i++)
-	{
-		cout << *(arr + i) << ' ';
-	}
-}
 
-int printMenu(int size, int *arr, int* arr2, int sellect)
-{
-	int index = 0;
-	
-
-	for (int i = 0; i < size; i++)
-	{
-		if (sellect == 1)
-		{
-			if (*(arr + i) % 2 != 0)
-			{
-				*(arr2 + index) = *(arr + i);
-				index++;
-			}
-		}
-		if (sellect == 2)
-		{
-			if (*(arr + i) % 2 == 0)
-			{
-				*(arr2 + index) = *(arr + i);
-				index++;
-			}
-		}
-	}
-
-	return index;
-}
-
-// 2 parne
 
 int main()
 {
-	cout << "Enter massive size: ";
-	int size;
-	cin >> size;
+	/*const int *p = nullptr;
+	int* s;
+	cout << p << endl;;
 
-	int* arr = new int[size];
-	
-	int* arr2 = new int[size];
-	
-	enterNum(size, arr);
-	printF(size, arr);
-	cout << endl << "Select delete num - 1 ne panrni / 2 parni: " << endl;
-	int sel;
-	cin >> sel;
-	
-	int index = printMenu(size, arr, arr2, sel);
-	printF(index, arr2);
+	int x = 5;
 
+	s = &x;
 
-	delete[] arr;
-	delete[] arr2;
+	p = &x;
+
+	cout << *p << endl;
+
+	int y = 7;
+
+	p = &y;
+
+	cout << *p << endl;*/
+
+	int a = 3;
+	int* const ptr = &a;
+
+	cout << ptr << endl;
+
+	
+	
 }
